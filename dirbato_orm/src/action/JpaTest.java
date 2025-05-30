4c5023d344c323f75ee7958427675824dc418de5
dir-FY25takamatsushunsuke <shunsuke.takamatsu.a196@dirbato.co.jp>
2025-05-30 15:11:29 +0900

    0530 lesson11end

--- dirbato_orm/src/action/JpaTest.java
+++ dirbato_orm/src/action/JpaTest.java
@@ -9,7 +9,15 @@ import util.DBUtil;
 public class JpaTest {
 
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
+        // EntityManagerのオブジェクトを作成
+        EntityManager em = DBUtil.creatEntityManager();
+        
+        //1件取得して名前を表示する
+        City city = em.find(City.class, 1);
+        System.out.println(city.getName());
+        
+        //EntityManagerの利用を終了する
+        em.close();
 
     }
 
